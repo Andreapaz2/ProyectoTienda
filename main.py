113af3293ab51d44ee93007fcdde6ac40b525ea0
Andreapaz2 <aagonzalez@scl.edu.gt>
2025-08-13 13:20:14 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import tkinter as tk 
-from views.header_view import cargar_header
-from views.productos_view import cargar_productos
+from Paneles.header_view import cargar_header
+from Paneles.productos_view import cargar_productos
+from views.login_view import *
 from views.login_view import cargar_login
+from views.dashboard import *
 
 ventana = tk.Tk()
 ventana.title("Mi tienda")
-ventana.geometry("1000x600")
-
+ventana.geometry("500x500")
 
 cargar_login(ventana)
 
+
 ventana. mainloop()
+#USUARIO para puruebas :  carlos.martinez@email.com | clave123  
+
